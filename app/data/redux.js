f3cf6057fb92af3463c586f85f5d7e8e2cdfd5ad
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-04-21 00:59:44 -0300

    Add new layout

--- app/data/redux.js
+++ app/data/redux.js
@@ -4,7 +4,6 @@ import { routerReducer } from 'react-router-redux';
 const rootReducer = combineReducers({
   /* eslint-disable */
   github: require('./github/redux').reducer,
-  posts: require('./posts/redux').reducer,
   routing: routerReducer,
   /* eslint-enable */
 });
--- app/scenes/Root/App.jsx
+++ app/scenes/Root/App.jsx
@@ -6,14 +6,20 @@ import '../../appearance/main.scss';
 export default function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="github">GitHub</Link>
-        </li>
-      </ul>
+      <nav className="main-menu">
+        <h1>React Fission</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="github">API Example</Link>
+          </li>
+          <li>
+            <a href="https://github.com/matheusmariano/react-fission" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </li>
+        </ul>
+      </nav>
       <Routes />
     </div>
   );
