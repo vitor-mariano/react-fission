f03792187739305567b18e89084ce4a080c40233
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-04-21 21:20:55 -0300

    Add toggle event to Check Button

--- app/components/CheckButton/index.jsx
+++ app/components/CheckButton/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import R from 'ramda';
 import './styles.scss';
 
 class CheckButton extends Component {
@@ -19,11 +20,31 @@ class CheckButton extends Component {
     return checked && <div className="mark" />;
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = R.pick(['checked'], props);
+  }
+
+  buttonDidClick() {
+    this.toggleCheck();
+  }
+
+  toggleCheck() {
+    this.setState({
+      checked: !this.state.checked,
+    });
+  }
+
   render() {
     return (
-      <div id="check-button">
-        {CheckButton.renderMark(this.props.checked)}
-      </div>
+      <button
+        id="check-button"
+        onClick={() => this.buttonDidClick()}
+        type="button"
+      >
+        {CheckButton.renderMark(this.state.checked)}
+      </button>
     );
   }
 }
