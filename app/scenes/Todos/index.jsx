d5bf12c76338bf2e372536bddace404f31dec226
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-04-22 14:08:51 -0300

    Improve Todos scene

--- app/scenes/Todos/index.jsx
+++ app/scenes/Todos/index.jsx
@@ -1,38 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Todos from './components/Todos/';
 import './styles.scss';
 
-class TodosScene extends Component {
-  render() {
-    return (
-      <div id="todos-scene">
-        <header className="page-header">
-          <h1>Todos</h1>
-        </header>
-        <section className="flex flex-center">
-          <Todos />
-        </section>
-      </div>
-    );
-  }
+function TodosScene() {
+  return (
+    <div id="todos-scene">
+      <header className="page-header">
+        <h1>Todos</h1>
+      </header>
+      <section className="flex flex-center">
+        <Todos />
+      </section>
+    </div>
+  );
 }
 
-TodosScene.propTypes = {
-};
-
-function mapStateToProps() {
-  return {
-  };
-}
-
-function mapDispatchToProps() {
-  return {
-  };
-}
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(TodosScene),
-);
+export default withRouter(TodosScene);
