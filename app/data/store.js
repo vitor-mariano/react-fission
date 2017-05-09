0f432c824202cb529b7555a003b1a4bbfaf3404e
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-05-08 22:18:06 -0300

    Add Redux Persist

--- app/data/store.js
+++ app/data/store.js
@@ -3,11 +3,14 @@ import createSagaMiddleware from 'redux-saga';
 import { browserHistory } from 'react-redux';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { autoRehydrate, persistStore } from 'redux-persist';
+import localforage from 'localforage';
 import R from 'ramda';
 import 'babel-polyfill';
 
 import rootReducer from './redux';
 import rootSaga from './sagas';
+import immutablePersistenceTransform from './transform';
 
 const defaultState = {};
 
@@ -19,6 +22,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [
   applyMiddleware(sagaMiddleware),
+  autoRehydrate(),
   reduxExtension,
 ];
 
@@ -30,6 +34,12 @@ const store = createStore(
     R.apply(compose),
   )(middlewares),
 );
+
+persistStore(store, {
+  storage: localforage,
+  whitelist: ['todos'],
+  transforms: [immutablePersistenceTransform],
+});
 
 sagaMiddleware.run(rootSaga);
 
