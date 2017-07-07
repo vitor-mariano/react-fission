c0aef7bf1c10e3ebb905de847dd4b2a5b75cd229
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-07-06 23:56:21 -0300

    Add tests for user repositories sagas

--- app/services/FixtureService.js
+++ app/services/FixtureService.js
@@ -4,5 +4,9 @@ export default {
     ok: true,
     data: require(`../fixtures/profile_request/${username}.json`),
   }),
+  userRepositoriesRequest: username => ({
+    ok: true,
+    data: require(`../fixtures/user_repositories_request/${username}.json`),
+  }),
   /* eslint-enable */
 };
