2002de5457a01d1ec1ebdb7f8702d9e8386ef5d8
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-07-30 23:44:01 -0300

    Fix Not Found page

--- app/scenes/NotFound/index.jsx
+++ app/scenes/NotFound/index.jsx
@@ -1,44 +1,41 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import Image from 'react-retina-image';
 import './styles.scss';
 
 class NotFoundScene extends Component {
+  static get contextTypes() {
+    return {
+      intl: PropTypes.object.isRequired,
+    };
+  }
+
   componentWillMount() {
-    document.title = 'Page not found - React Fission';
+    document.title = this.formatMessage({ id: 'not_found.page_title' });
+  }
+
+  formatMessage(props) {
+    return this.context.intl.formatMessage(props);
   }
 
   render() {
     return (
-      <div>
-        <section className="jumbotron">
-          <img
-            alt="Not Found"
-            src="images/not-found.png"
-          />
-          <h2 className="jumbotron__subtitle">
-            <FormattedMessage id="not_found.title" />
-          </h2>
-        </section>
-      </div>
+      <section className="not-found-page text-center">
+        <FormattedMessage id="not_found.page_title">
+          {message => (
+            <Image
+              alt={message}
+              src="images/not-found.png"
+            />
+          )}
+        </FormattedMessage>
+        <h2 className="display-3 not-found-page__title">
+          <FormattedMessage id="not_found.title" />
+        </h2>
+      </section>
     );
   }
 }
 
-NotFoundScene.propTypes = {
-};
-
-function mapStateToProps() {
-  return {
-  };
-}
-
-function mapDispatchToProps() {
-  return {
-  };
-}
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(NotFoundScene),
-);
+export default NotFoundScene;
